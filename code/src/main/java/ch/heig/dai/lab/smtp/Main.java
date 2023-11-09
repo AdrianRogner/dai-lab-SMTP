cfaa1f7ae758cadc144cf7becb77cd7e2a08974f
AdrianRogner <adrian.rogner@heig-vd.ch>
2023-11-09 11:25:15 +0100

    Début

---
+++ code/src/main/java/ch/heig/dai/lab/smtp/Main.java
@@ -0,0 +1,9 @@
+package code.src.main.java.ch.heig.dai.lab.smtp;
+import java.net.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("test");
+    }
+}
