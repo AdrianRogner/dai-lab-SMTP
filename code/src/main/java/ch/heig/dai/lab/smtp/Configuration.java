54459d5fccf2caa72ae7b844c459c1a282a0d0e7
AdrianRogner <adrian.rogner@heig-vd.ch>
2023-12-03 18:56:41 +0100

    Configuration ongoing

--- code/src/main/java/ch/heig/dai/lab/smtp/Configuration.java
+++ code/src/main/java/ch/heig/dai/lab/smtp/Configuration.java
@@ -5,26 +5,69 @@ import java.io.*;
 import java.nio.charset.*;
 import java.sql.SQLData;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Pattern;
 
 public class Configuration {
     File victims;
     File messages;
     int numberOfGroups;
+    int nbrPeoplePerGroup;
 
-    Configuration(File victims, File messages, int numberOfGroups){
+    Configuration(File victims, File messages, int numberOfGroups, int nbrPeoplePerGroup){
         this.victims = victims;
         this.messages = messages;
+        if(numberOfGroups < 1){
+            throw new IllegalArgumentException("Number of groups inferior to 1");
+        }
         this.numberOfGroups = numberOfGroups;
+        this.nbrPeoplePerGroup = nbrPeoplePerGroup;
     }
 
-    public String[] configureString(File f){
-        Charset charset = getEncoding(f);
+    public String[] readVictims(){
+        Charset charset = getEncoding(this.victims);
         String content;
         if(charset != null) {
-            content = readFile(f, charset);
-            return content.split("$$$");
+            content = readFile(this.victims, charset);
+            return content.trim().split("\\s*,+\\s*,*\\s*"); // pour éviter les espaces
         }
         return null;
+    }
+
+    public String[] readMessages(){
+        Charset charset = getEncoding(this.messages);
+        String content;
+        if(charset != null) {
+            content = readFile(this.messages, charset);
+            return content.split("---");
+        }
+        return null;
+    }
+
+    public boolean validateEmail(String[] emails){
+        String regexPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        for(int i = 0; i < emails.length; i++) {
+            if(!Pattern.compile(regexPattern).matcher(emails[i]).matches()){
+                return false;
+            }
+        }
+         return true;
+    }
+
+    public String[][] formGroup(String[] group){
+
+        String[][] str = new String[this.numberOfGroups][this.nbrPeoplePerGroup];
+        int peopleInserted = 0;
+        for(int i = 0; i < this.numberOfGroups; i++){
+            for(int j = 0; j < this.nbrPeoplePerGroup; j++){
+                if(group.length <= peopleInserted){
+                    return str;
+                }
+                str[i][j] = group[peopleInserted++];
+
+            }
+        }
+        return str;
     }
 
     private String readFile(File file, Charset encoding) {
@@ -45,7 +88,7 @@ public class Configuration {
     }
 
     private Charset getEncoding(File file) {
-        // TODO: implement the method body here
+
         String f = file.getName();
         String[] words = f.split("\\.");
 
